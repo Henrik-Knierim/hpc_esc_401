074e7c5aab65dfb957670501cd7574b61c60eb33
Henrik Knierim <henrik@MacBook-Pro-von-Henrik.local>
2024-10-25 12:19:22 +0200

    exercise 6

---
+++ exercise_session_06/ring_and_deadlocks/ring.c
@@ -0,0 +1,30 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes and rank of the process
+    int size, my_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    int my_sum = 0;
+
+    int send_rank = my_rank;  // Send    buffer
+    int recv_rank = 0;        // Receive buffer
+
+    // Compute the ranks of left/right neighbours 
+    int left_rank, right_rank;
+
+    // Loop over the number of processes
+    //     send to right, receive from left
+    //     update the send buffer
+    //     update the local sum
+
+    printf("I am processor %d out of %d, and the sum is %d\n", my_rank, size, my_sum);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
